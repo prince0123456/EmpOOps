eb769bc2e4ea72cdc64de191f558f90eebcaf7e0
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-11 16:04:25 +0530

    Emp OOPs

---
+++ EmployeeOOPs/EmployeeOOPs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeOOPs // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EmpWage emp = new EmpWage();
+            EmpWage.empwage();
+        }
+    }
+}
